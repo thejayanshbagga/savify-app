2789be2b4b9c5a4eaa845f704a9058d98796d885
Vaishnavi Gudimella <118631877+eventidesun@users.noreply.github.com>
2025-07-23 12:51:26 -0400

    Added show password option

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,13 +3,14 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Feather } from '@expo/vector-icons';
 
-export default function LoginScreen() {
+export default function LoginScreen({navigation}) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const handleLogin = () => {
-    // alert('✅ Logged in (hook up API)');
     navigation.navigate('Home');
   };
 
@@ -17,29 +18,43 @@ export default function LoginScreen() {
     <SafeAreaView style={tw`flex-1 bg-[#5C8EDC]`}
 >
       <View style={tw`flex-1 justify-center px-6`}>
-        <Text style={tw`text-2xl font-bold text-center text-primary mb-6`}>Welcome Back to Savify</Text>
+        <Text style={tw`text-5xl font-bold text-left text-primary mb-6`}>Welcome back! Glad to see you again!</Text>
 
+        {/*Email address input*/}
         <TextInput
-          style={tw`border border-gray-300 p-3 mb-4 rounded`}
-          placeholder="Email"
+          style={tw`border border-gray-300 p-3 mb-4 rounded-2 bg-white`}
+          placeholder="Email Address"
           keyboardType="email-address"
           value={email}
           onChangeText={setEmail}
         />
+
+        {/*Password input with show password eye icon*/}
         <TextInput
-          style={tw`border border-gray-300 p-3 mb-4 rounded`}
+          style={tw`border border-gray-300 p-3 mb-4 rounded-2 bg-white`}
           placeholder="Password"
-          secureTextEntry
+          secureTextEntry={!showPassword}
           value={password}
           onChangeText={setPassword}
         />
+        <TouchableOpacity
+            onPress={() => setShowPassword(!showPassword)}
+            style={tw`absolute right-9 top-102.5`}
+        >
+          <Feather name={showPassword ? 'eye-off' : 'eye'} size={22} color="#333" />
+        </TouchableOpacity>
+
+        {/*Login button*/}
         <TouchableOpacity style={tw`bg-primary py-3 rounded`} onPress={handleLogin}>
-          <Text style={tw`text-center text-white font-bold`}>Log In</Text>
+          <Text style={tw`border border-white-300 p-3 rounded-2 bg-black text-center text-white font-bold`}>
+            Log In
+          </Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={tw`mt-4 border border-primary py-3 rounded`}>
           <Text style={tw`text-center text-primary font-bold`}>Continue with Google</Text>
         </TouchableOpacity>
+
       </View>
     </SafeAreaView>
   );
