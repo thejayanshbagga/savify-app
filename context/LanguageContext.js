d81a8fda275b9c404917d63bb983c8f2cd6527c4
Adit <aditbhimani007@gmail.com>
2025-08-01 22:18:23 -0400

    Added code that remembers which language was used last.

--- context/LanguageContext.js
+++ context/LanguageContext.js
@@ -1,13 +1,35 @@
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import * as Localization from 'expo-localization';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { translations } from '../constants/translations';
 
 const LanguageContext = createContext();
 
 export const LanguageProvider = ({ children }) => {
   const [language, setLanguage] = useState('EN');
 
-  // Accept an argument to explicitly set a language
-  const toggleLanguage = (newLang) => {
-    setLanguage(newLang); // instead of just toggling between EN/FR
+  useEffect(() => {
+    const initLanguage = async () => {
+      // Check stored language first
+      const storedLang = await AsyncStorage.getItem('preferredLanguage');
+      if (storedLang && translations[storedLang]) {
+        setLanguage(storedLang);
+      } else {
+        // Auto-detect from device locale
+        const locale = Localization.locale.split('-')[0].toUpperCase();
+        const deviceLang = translations[locale] ? locale : 'EN';
+        setLanguage(deviceLang);
+
+        console.log("Detected language:", Localization.locale);
+      }
+    };
+
+    initLanguage();
+  }, []);
+
+  const toggleLanguage = async (newLang) => {
+    setLanguage(newLang);
+    await AsyncStorage.setItem('preferredLanguage', newLang);
   };
 
   return (
@@ -16,6 +38,5 @@ export const LanguageProvider = ({ children }) => {
     </LanguageContext.Provider>
   );
 };
-
 
 export const useLanguage = () => useContext(LanguageContext);
