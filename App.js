6cac56a65c52f8f9fbb40ba3b9d880bbddd393fe
Adit <aditbhimani007@gmail.com>
2025-09-24 18:58:11 -0400

    Fixed issue related to skip for now not working.

--- App.js
+++ App.js
@@ -1,41 +1,3 @@
-// import React from 'react';
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-
-// import LandingScreen from './screens/LandingScreen';
-// import SignupScreen from './screens/SignupScreen';
-// import LoginScreen from './screens/LoginScreen';
-// import CalculatorScreen from './screens/CalculatorScreen';
-// import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
-// import MainTabs from './navigation/MainTabs';
-// import { LanguageProvider } from './context/LanguageContext';
-
-// // navigation from score screen to redeem page
-// import ScoreScreen from './screens/ScoreScreen';
-// import RedeemScreen from './screens/RedeemScreen';
-
-// const Stack = createStackNavigator();
-
-// export default function App() {
-//   return (
-//     <LanguageProvider>
-//     <NavigationContainer>
-//       <Stack.Navigator initialRouteName="Landing" screenOptions={{ headerShown: false }}>
-//         <Stack.Screen name="Landing" component={LandingScreen} />
-//         <Stack.Screen name="Login" component={LoginScreen} />
-//         <Stack.Screen name="Signup" component={SignupScreen} />
-//         <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
-//         <Stack.Screen name="Calculator" component={CalculatorScreen} />
-//         <Stack.Screen name="Score" component={ScoreScreen} />
-//         <Stack.Screen name="Redeem" component={RedeemScreen} />
-//         <Stack.Screen name="MainTabs" component={MainTabs} />
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//     </LanguageProvider>
-//   );
-// }
-
-
 // App.js
 import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
@@ -76,6 +38,7 @@ function RootNavigator() {
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Signup" component={SignupScreen} />
           <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
+          <Stack.Screen name="MainTabs" component={MainTabs} />
         </>
       )}
     </Stack.Navigator>
